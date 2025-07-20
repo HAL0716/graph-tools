48ab882ab1d381a8dbe3de4891bdfa37c6193509
HAL0716 <207191136+HAL0716@users.noreply.github.com>
2025-07-20 15:52:04 +0900

    Add Calcuration Max Eigenvalues

--- src/main.cpp
+++ src/main.cpp
@@ -2,30 +2,15 @@
 #include <string>
 #include <vector>
 #include <utility>
-#include <unordered_map>
 #include <Eigen/Dense>
 #include "Common.hpp"
 #include "NodeEncoder.hpp"
-
-std::ostream& operator<<(std::ostream& os, const std::pair<std::string, int>& node) {
-    os << "(" << node.first << "," << node.second << ")";
-    return os;
-}
-
-std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<int>>& matrix) {
-    for (const auto& raw : matrix) {
-        for (int val : raw)
-            os << val << " ";
-        os << std::endl;
-    }
-    return os;
-}
 
 int main() {
     constexpr int Q = 3;
     constexpr int T = 3;
     constexpr int K = 4;
-    const std::string fword = "0110";
+    const std::string fword = "0001";
 
     std::vector<std::pair<std::string, int>> nodes;
     for (int i = 0; i <= K; ++i)
@@ -39,28 +24,35 @@ int main() {
     const auto edgeLbls = SYMBOLS.substr(0, Q);
     const int nodeCnt = enc.size();
 
-    std::vector<std::vector<int>> adjMatrix(nodeCnt, std::vector<int>(nodeCnt, 0));
+    Eigen::MatrixXd adjMat = Eigen::MatrixXd::Zero(nodeCnt, nodeCnt);
 
     for (const auto& stNode : nodes) {
         if (stNode == std::pair<std::string, int>{fword, 0}) continue;
-        const int stId = enc.encode(stNode);
+        int stId = enc.encode(stNode);
 
         for (const auto& lbl : edgeLbls) {
-            const std::string tgtLbl = stNode.first + lbl;
+            std::string tgtLbl = stNode.first + lbl;
 
             for (int i = 0; i <= static_cast<int>(tgtLbl.length()); ++i) {
-                const std::pair<std::string, int> edNode{tgtLbl.substr(i), (stNode.second + i) % T};
-                const int edId = enc.encode(edNode);
+                std::pair<std::string, int> edNode{tgtLbl.substr(i), (stNode.second + i) % T};
+                int edId = enc.encode(edNode);
 
                 if (edId < nodeCnt) {
-                    adjMatrix[stId][edId]++;
+                    adjMat(stId, edId) += 1;
                     break;
                 }
             }
         }
     }
 
-    std::cout << adjMatrix << std::endl;
+    Eigen::EigenSolver<Eigen::MatrixXd> solver(adjMat);
+    const auto& eigvals = solver.eigenvalues();
+
+    double maxReal = eigvals[0].real();
+    for (int i = 1; i < eigvals.size(); ++i)
+        maxReal = std::max(maxReal, eigvals[i].real());
+
+    std::cout << std::endl << fword << " : " << maxReal << std::endl;
 
     return 0;
 }
